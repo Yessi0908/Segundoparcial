955ae5a94ef5e03d89741b3161337498ae584097
Yessi 0908 <jramirezs26@miumg.edu.gt>
2026-05-02 17:43:26 -0600

    Add Segundo Parcial subtitle

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Juana Yessenia Ramirez Santiago</h1>'
+    return '<h1>Juana Yessenia Ramirez Santiago</h1><h2>Segundo Parcial</h2>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
